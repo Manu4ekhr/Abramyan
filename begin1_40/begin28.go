aa3107cb4d973619dc4044c977807b35376cdde0
Manu4ekhr <14-manuchehr@mail.ru>
2023-11-28 13:45:37 +0500

    Begin28

--- begin1_40/begin28.go
+++ begin1_40/begin28.go
@@ -1,1 +1,17 @@
 package begin1_40
+
+import "fmt"
+
+func Begin28(a int) {
+	var x, y int
+	x = a * a
+	fmt.Printf("А в степени 2: %v \n", x)
+	y = a * x
+	fmt.Printf("А в степени 3: %v \n", y)
+	y = y * x
+	fmt.Printf("А в степени 5: %v \n", y)
+	y = y * y
+	fmt.Printf("А в степени 10: %v \n", y)
+	y = y * x * a
+	fmt.Printf("А в степени 15: %v \n", y)
+}
