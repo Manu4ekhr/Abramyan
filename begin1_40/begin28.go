8c2780b369f72cfc8944996c6602dbf91184ba0c
Manu4ekhr <14-manuchehr@mail.ru>
2023-11-29 16:24:27 +0500

    Begin29

--- begin1_40/begin28.go
+++ begin1_40/begin28.go
@@ -3,15 +3,15 @@ package begin1_40
 import "fmt"
 
 func Begin28(a int) {
-	var x, y int
+	var x int
 	x = a * a
 	fmt.Printf("А в степени 2: %v \n", x)
-	y = a * x
-	fmt.Printf("А в степени 3: %v \n", y)
-	y = y * x
-	fmt.Printf("А в степени 5: %v \n", y)
-	y = y * y
-	fmt.Printf("А в степени 10: %v \n", y)
-	y = y * x * a
-	fmt.Printf("А в степени 15: %v \n", y)
+	a = x * a
+	fmt.Printf("А в степени 3: %v \n", a)
+	x = x * a
+	fmt.Printf("А в степени 5: %v \n", x)
+	a = x * x
+	fmt.Printf("А в степени 10: %v \n", a)
+	x = a * x
+	fmt.Printf("А в степени 15: %v \n", x)
 }
---
+++ begin1_40/begin29.go
@@ -0,0 +1,14 @@
+package begin1_40
+
+import (
+	"fmt"
+	"math"
+)
+
+//const Pi = 3.14
+
+func Begin29(a float64) {
+	var r float64
+	r = math.Pi / 180 * a
+	fmt.Println(r)
+}
